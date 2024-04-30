3f2ba81afb8cd70ca4a9a8baa15bc27d47b1719d
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-30 11:41:09 +0530

    [Rohit] Add: Welcome message

---
+++ User_Registration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace User_Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+        }
+    }
+}
