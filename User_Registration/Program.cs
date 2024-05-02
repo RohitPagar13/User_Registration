61bbf72e7869366c55b4ecddad067966e25b3f99
Rohit Pagar <rohitpagar13@gmail.com>
2024-05-02 17:09:55 +0530

    [Rohit] Add: password minimum 8 characters

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -7,24 +7,24 @@ namespace User_Registration
         static void CreateUser()
         {
             Regex exp1 = new Regex(@"\b[A-Z]+\w{2,}");
-
             Console.WriteLine("Enter First Name: ");
             string Fname = Console.ReadLine();
 
             Regex exp2 = new Regex(@"\b[A-Z]+\w{2,}");
-
             Console.WriteLine("Enter Last Name: ");
             string Lname = Console.ReadLine();
 
             Regex exp3 = new Regex(@"\b\w+\.?\w+?\@[a-z]+\.[a-z]+((?:.)[a-z]{1,})*");
-
             Console.WriteLine("Enter EMail: ");
             string email = Console.ReadLine().ToLower();
 
             Regex exp4 = new Regex(@"\b((\d{0,3})\s)?\d{6,10}");
-
             Console.WriteLine("Enter Mobile no: ");
             string no = Console.ReadLine();
+
+            Regex exp5 = new Regex(@"\b[^\s].[^\s]{8,}}");
+            Console.WriteLine("Enter Password: ");
+            string pass = Console.ReadLine();
 
             if (!exp1.IsMatch(Fname))
             {
