648d0b706de4fe1ece9c81653d3fa2cfca5f9945
Rohit Pagar <rohitpagar13@gmail.com>
2024-05-02 15:08:44 +0530

    [Rohit] Add: Validations for user first name

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace User_Registration
+﻿using System.Text.RegularExpressions;
+
+namespace User_Registration
 {
     internal class Program
     {
+        static void CreateUser()
+        {
+            Regex exp1 = new Regex(@"\b[A-Z]+\w{2,}");
+
+            Console.WriteLine("Enter First Name: ");
+            string Fname = Console.ReadLine();
+
+            if (exp1.IsMatch(Fname))
+            {
+                User user = new User(Fname);
+                Console.WriteLine("User Created");
+            }
+            else
+            {
+                Console.WriteLine("Enter valid user First Name");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration");
+            CreateUser();
         }
     }
 }
---
+++ User_Registration/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    internal class User
+    {
+        public string FName { get; }
+
+        public User(string fName)
+        {
+            this.FName = fName;
+        }
+
+    }
+}
